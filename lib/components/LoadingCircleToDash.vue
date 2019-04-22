4044426060e53db2dbdffbad328b4ea59b114499
OwnGhy <919986426@qq.com>
2019-04-22 13:15:18 +0800

    添加circle-to-dash 和 circle-ridge类型

--- lib/components/LoadingCircleToDash.vue
+++ lib/components/LoadingCircleToDash.vue
@@ -1,9 +1,9 @@
 <template>
-  <div :class="`fun-loading-circle-to-dot fun-loading-circle-to-dot-${size}`" :style="loadingStyle"></div>
+  <div :class="`fun-loading-circle-to-dash fun-loading-circle-to-dash-${size}`" :style="loadingStyle"></div>
 </template>
 <script>
   export default {
-    name: 'FunLoadingCircleToDot',
+    name: 'FunLoadingCircleToDash',
     props: {
       size: {
         type: String,
@@ -27,13 +27,13 @@
 <style lang="less">
   @import "./style/_variables.less";
 
-  @loading-class: @{fun-loading-prefix}-circle-to-dot;
+  @loading-class: @{fun-loading-prefix}-circle-to-dash;
 
   .@{loading-class} {
     border-radius: 50%;
     border-style: solid;
     border-color: @fun-loading-color @fun-loading-light-color;
-    animation: rotate_to_dot 1.5s infinite;
+    animation: rotate_to_dash 1.5s infinite;
 
     &.@{loading-class}-small {
       width: 12px;
@@ -48,18 +48,18 @@
     }
 
     &.@{loading-class}-large {
-      width: 26px;
-      height: 26px;
-      border-width: 12px;
+      width: 30px;
+      height: 30px;
+      border-width: 10px;
     }
 
-    @keyframes rotate_to_dot {
+    @keyframes rotate_to_dash {
       50%{
         transform:rotate(180deg) scale(1.3);
-        border-style:dotted;
+        border-style: dashed;
       }
       100%{
-        transform:rotate(360deg) scale(1);
+        transform:rotate(360deg) scale(0.9);
       }
     }
   }
---
+++ src/docs/circletodash.md
@@ -0,0 +1,20 @@
+---
+title: circle-to-dash
+desc: loading type of circle-to-dash.
+---
+
+```vue
+<template>
+  <vue-fun-loading type='circle-to-dash' />
+</template>
+
+<script>
+import VueFunLoading from 'vue-fun-loading'
+
+export default {
+  components: {
+    VueFunLoading
+  }
+}
+</script>
+```
